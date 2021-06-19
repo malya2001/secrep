a4a4a35a146c68471f99c5ae21d5e9531a2f942b
Dumitrash <saratinean1.9@gmail.com>
2021-06-19 10:24:40 +0300

    sec

---
+++ docker/server.js
@@ -0,0 +1,15 @@
+var http = require('http');
+var startTime;
+var host;
+var handleRequest = function(request, response) {
+  response.setHeader('Content-Type', 'text/plain');
+  response.writeHead(200);
+  response.write("Hello World , este incercarea II! | Ruleaza pe :");
+  response.write(host);
+  response.end(" v=2\n");
+  console.log("Running On:" , host);
+}
+var www = http.createServer(handleRequest);
+www.listen(8080,function () {
+    host = process.env.HOSTNAME;
+});
